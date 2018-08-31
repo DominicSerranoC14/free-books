522688612180b2f9a1220dc3070e31da1c286110
DominicSerrano <dmsrojo@gmail.com>
2018-08-31 16:37:12 -0500

    Removed config.js

--- src/Services/Firebase/config.js
+++
@@ -1,8 +0,0 @@
-export default {
-    apiKey: "AIzaSyDddkRkVXKmpoO6pCohVsXMwb7A2YmgFA8",
-    authDomain: "free-books-eb916.firebaseapp.com",
-    databaseURL: "https://free-books-eb916.firebaseio.com",
-    projectId: "free-books-eb916",
-    storageBucket: "free-books-eb916.appspot.com",
-    messagingSenderId: "753709363182"
-};
